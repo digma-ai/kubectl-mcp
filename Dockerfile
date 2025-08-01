f62f2ad8a4ea6d0b910a84cef94b1c95210c55b4
Asaf Chen <achen@digma.ai>
2025-08-01 16:28:22 -0400

    infra

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+﻿FROM python:3.11-slim
+
+# Set work directory inside the container
+WORKDIR /app
+
+# Install system dependencies (if needed)
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install Python dependencies
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the application code into the container
+COPY src .
+
+ENV PYTHONPATH=/app \
+    PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1
+
+EXPOSE ${PORT}
+
+# Define the default command to run the app
+CMD ["python", "mcp_server.py"]
---
+++ src/mcp_server.py
@@ -0,0 +1,28 @@
+﻿import argparse
+import os
+
+from mcp.server.fastmcp import FastMCP
+
+
+def add_tools(mcp: FastMCP):
+
+    @mcp.tool()
+    def echo_tool(text: str) -> str:
+        """Echo the input text"""
+        return text
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--transport", type=str, choices=["sse", "stdio", "streamable-http"], default="sse")
+    parser.add_argument("-p", "--port", type=int, default=8000)
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+
+    args = parser.parse_args()
+
+    port = os.getenv("PORT", args.port)
+    host = os.getenv("HOST", args.host)
+
+    mcp = FastMCP("Kubectl Mcp Server", port=port, host=host)
+    add_tools(mcp=mcp)
+    mcp.run(transport=args.transport)
