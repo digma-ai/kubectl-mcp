511ae0a1014e3a27a9f82a5194a7bb7073ed2a6b
Roni Dover <roni.dover@gmail.com>
2025-08-07 22:45:18 -0700

    unsuppored watch argument

--- src/kubectl_process.py
+++ src/kubectl_process.py
@@ -11,6 +11,8 @@ def run_kubectl(command_args: list[str], *, stdin: str = None) -> str:
     try:
         command_args = command_args or []
         command_args = [x.strip() for x in command_args]
+        if ("-w" in command_args) or ("--watch" in command_args):
+            return "Unable to run kubectl with the --watch or -w flags, please modify your call to exclude this parameter"
         command_items = [KUBECTL_NAME, *command_args]
         result = subprocess.run(args=command_items, input=stdin, text=True, check=True, capture_output=True,timeout=PROCESS_TIMEOUT)
         return result.stdout
